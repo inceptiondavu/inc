7eef5301cd90d7c9e854971445e07f118dc6e2d6
inceptiondavu <official.work.sawan@gmail.com>
2016-09-12 20:03:49 +0530

    Update events.js

--- events.js
+++ events.js
@@ -1,8 +1,19 @@
 var main = function(){
 
+    var i = 0;
+
     $(".event-info").click(function(){
         $(this).find(".desc-hide").toggle();
+        if( i === 0){
+            $(this).find(".arrow").html("&#9650;");
+            i = 1;
+        }
+        else{
+            $(this).find(".arrow").html("&#9660;");
+            i = 0;
+        }
     });
+    
 };
 
 $(document).ready(main);
